d1c9d8dd5bfc6c66eb9f2ab257943c2ed56ca351
dgibn <dg1803@yahoo.com>
2022-04-26 23:27:33 +0530

    Revise

--- LHC/mainapp/models.py
+++ LHC/mainapp/models.py
@@ -3,6 +3,7 @@ class bookings(models.Model):
     EventName=models.CharField(max_length=200,default="John Doe")
     Venue=models.CharField(max_length=1,default="NULL")
     Room=models.CharField(max_length=6,default="Null")
-    Date=models.CharField(max_length=10,default="01-01-0001")
-    Starttime=models.IntegerField(default=0000)
-    endtime=models.IntegerField(default=0000)
+    Date=models.DateField()
+    Starttime=models.TimeField()
+    endtime=models.TimeField()
+
