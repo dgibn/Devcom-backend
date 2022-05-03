c37d4355a440af594f96d990ac2474e9ce85cc23
dgibn <dg1803@yahoo.com>
2022-05-03 14:42:41 +0530

    Revised

--- LHC/mainapp/models.py
+++ LHC/mainapp/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 class bookings(models.Model):
     EventName=models.CharField(max_length=200,default="John Doe")
-    Venue=models.CharField(max_length=1,default="NULL")
-    Room=models.CharField(max_length=6,default="Null")
-    Date=models.DateField()
-    Starttime=models.TimeField()
-    endtime=models.TimeField()
+    Venue=models.CharField(max_length=1,null=False)
+    Room=models.CharField(max_length=6,null=False)
+    Date=models.DateField(null=False)
+    Starttime=models.TimeField(null=False)
+    endtime=models.TimeField(null=False)
 
